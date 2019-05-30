fb53204b157b611b1a1cf0dfb9843212a815afff
Hira797 <48818522+Hira797@users.noreply.github.com>
2019-05-30 16:36:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function showAlert() {
+
+    console.log('Alert # 1');
+    console.log('Alert # 2');
+    console.log('Alert # 3');
+    console.log('Alert # 4');
+    console.log('Alert # 5');
+}
+
+function secondFunc() {
+
+    alert('Aniqa Mukhtar');
+}
